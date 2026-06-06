8f6facec5a698c6678a427841c9660d7c05c71e6
Carnage098 <polairebleu7@gmail.com>
2026-06-06 16:23:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
+WARNING_THRESHOLD = -3
+PROBATION_THRESHOLD = -6
+SUSPENDED_THRESHOLD = -10
+BANNED_THRESHOLD = -15
 
+INFRACTIONS = {
+    "TRICHE": "Triche",
+    "STREAMHACK": "Streamhack",
+    "INSULTE": "Insulte",
+    "TRASHTALK": "Trash-talk",
+    "ANTI_SPORTIF": "Comportement anti-sportif",
+    "RETARD": "Retard",
+    "ABSENCE": "Absence",
+    "REFUS_MATCH": "Refus de jouer",
+    "AUTRE": "Autre"
+}
+
+GRAVITIES = {
+    "MINEURE": -1,
+    "MOYENNE": -2,
+    "GRAVE": -3,
+    "CRITIQUE": -5
+}
