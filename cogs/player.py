d2e075a57eea6e4b1f32c75c93a606e9cdef535d
Carnage098 <polairebleu7@gmail.com>
2026-06-06 16:26:23 +0200

    Update player.py

--- cogs/player.py
+++ cogs/player.py
@@ -1,1 +1,35 @@
+from discord.ext import commands
+from discord import app_commands
+import discord
 
+class Player(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    player = app_commands.Group(
+        name="player",
+        description="Commandes joueur"
+    )
+
+    @player.command(
+        name="profile",
+        description="Voir son profil"
+    )
+    async def profile(
+        self,
+        interaction: discord.Interaction
+    ):
+
+        embed = discord.Embed(
+            title="Profil joueur",
+            description=f"Joueur : {interaction.user.mention}"
+        )
+
+        await interaction.response.send_message(
+            embed=embed,
+            ephemeral=True
+        )
+
+async def setup(bot):
+    await bot.add_cog(Player(bot))
